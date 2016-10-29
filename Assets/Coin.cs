80a40951277ae730bd4d516091b051056cfe7c77
jona8690 <jona8690@edu.eal.dk>
2016-10-29 16:05:31 +0200

    Coins change material to fit value

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Coin : MonoBehaviour {
 	public int Value = 0;
 	public Material[] Materials;
+
+	private int OldValue;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,6 +13,9 @@ public class Coin : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(OldValue != Value) {
+			gameObject.GetComponent<Renderer>().material = Materials[Value];
+			this.OldValue = Value;
+		}
 	}
 }
